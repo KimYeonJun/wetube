dfd5e5551e3c9352c5926b43d8241a7e8cfdf844
KimYeonJun <duswns1783@naver.com>
2020-07-16 22:01:00 +0900

    add controller

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
+import { users, editProfile, changePassword, userDetail } from "../controllers/userController";
 
 const userRouter = express.Router();
 
+userRouter.get(routes.users, (req,res) => users);
+userRouter.get(routes.editProfile,(req, res) => editProfile);
+userRouter.get(routes.changePassword,(req, res) => changePassword);
+userRouter.get(routes.userDetail, (req,res) => userDetail);
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import routes from "../routes";
+import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos , (req,res) => videos);
+videoRouter.get(routes.upload, (req,res) => upload);
+videoRouter.get(routes.videoDetail, (req,res) => videoDetail);
+videoRouter.get(routes.editVideo, (req,res) => editVideo);
+videoRouter.get(routes.deleteVideo, (req,res) => deleteVideo);
 
 export default videoRouter;
