72f4018efc0eb862c53b49690bb9e800a371d71f
KimYeonJun <duswns1783@naver.com>
2020-07-20 21:13:28 +0900

    Add Home Controller

---
+++ db.js
@@ -0,0 +1,51 @@
+export const videos =[
+    {
+        id:324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:121212,
+            name:"KimYeonJun",
+            email:"duswns1783@naver.com",
+        }
+    },
+    {
+        id:121212,
+        title: 'Video aaaa',
+        description: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:121212,
+            name:"KimYeonJun",
+            email:"duswns1783@naver.com",
+        }
+    },
+    {
+        id:34343434,
+        title: 'Video bbbb',
+        description: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:121212,
+            name:"KimYeonJun",
+            email:"duswns1783@naver.com",
+        }
+    },
+    {
+        id:56565656,
+        title: 'Video cccc',
+        description: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:121212,
+            name:"KimYeonJun",
+            email:"duswns1783@naver.com",
+        }
+    }
+
+]
