17e9d9cb6da1c903b99f1dfbd2cd2ad954c8aacc
KimYeonJun <duswns1783@naver.com>
2020-07-20 20:08:36 +0900

    Add HTML Source

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,7 +4,7 @@ import { users, editProfile, changePassword, userDetail } from "../controllers/u
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
+//userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile,editProfile);
 userRouter.get(routes.changePassword,changePassword);
 userRouter.get(routes.userDetail,userDetail);
