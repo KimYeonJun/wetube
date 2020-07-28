8f968328d8060a8f59aab614d74cf523264d5a8b
KimYeonJun <duswns1783@naver.com>
2020-07-28 16:32:30 +0900

    Add Upload Video

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 import routes from "./routes";
-const multerVideo = multer({dest: "videos/"});
+const multerVideo = multer({dest: "uploads/videos/"});
 export const localsMiddleware = (req,res,next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
