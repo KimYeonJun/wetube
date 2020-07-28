a2e8eb305ed5c12c88c8865f5ef1aaadffbc4220
KimYeonJun <duswns1783@naver.com>
2020-07-28 15:36:00 +0900

    Add Upload Video

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan("dev"));
 
 app.use(localsMiddleware);
