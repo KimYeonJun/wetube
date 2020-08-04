b13ce977f5fcf3c7d6a798c809d15c7d1b3e63e3
KimYeonJun <duswns1783@naver.com>
2020-08-04 21:19:19 +0900

    Add Github Authentication

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const SEARCH = "/search";
 
 //Users
 
-const USERS ="/users";
+const USERS = "/users";
 const EDIT_PROFILE = "/edit-profile";
 const USER_DETAIL = "/:id";
 const CHANGE_PASSWORD = "/change-password";
@@ -20,46 +20,51 @@ const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
-const routes = {
-    home: HOME,
-    join: JOIN,
-    login: LOGIN,
-    logout: LOGOUT,
-    search: SEARCH,
-    users: USERS,
-    userDetail: (id) => {
-        if(id){
-            return `/users/${id}`;
-        } else{
-            return USER_DETAIL;
-        }
+//Github
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
 
-    },
-    editProfile: EDIT_PROFILE,
-    changePassword: CHANGE_PASSWORD,
-    videos: VIDEOS,
-    upload: UPLOAD,
-    videoDetail: (id) => {
-        if(id){
-            return `/videos/${id}`;
-        } else{
-            return VIDEO_DETAIL;
-        }
-    },
-    editVideo: (id)=>{
-        if(id){
-            return `/videos/${id}/edit`;
-        } else{
-            return EDIT_VIDEO;
-        }
-    },
-    deleteVideo: (id)=>{
-        if(id){
-            return `/videos/${id}/delete`;
-        } else{
-            return DELETE_VIDEO;
-        }
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: (id) => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
     }
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: (id) => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  editVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
+    }
+  },
+  deleteVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  },
+  github: GITHUB,
+  githubCallback: GITHUB_CALLBACK,
 };
 
 export default routes;
